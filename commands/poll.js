a2058c827b3a26caf5d87d9f7bbc32bfd6938380
Thibault <thibaultchatillon@gmail.com>
2024-03-23 23:11:50 +0100

    working but not beautifull yet

---
+++ commands/poll.js
@@ -0,0 +1,61 @@
+import { SlashCommandBuilder, EmbedBuilder, Colors } from 'discord.js';
+
+// Emojis representing letters (a, b, c, ...)
+const emojis = ['🇦', '🇧', '🇨', '🇩', '🇪', '🇫', '🇬', '🇭', '🇮', '🇯'];
+const letters = emojis.map((emoji, index) => String.fromCharCode(97 + index));
+
+
+export const data = new SlashCommandBuilder()
+    .setName('poll')
+    .setDescription('Creates a poll')
+    .addStringOption(option => option.setName('question').setDescription('The question to ask').setRequired(true));
+
+for (let i = 0; i < emojis.length; i++) {
+    data.addStringOption(option => option.setName(`choice_${letters[i]}`).setDescription(`The ${i + 1}th option`));
+}
+
+export async function execute(interaction) {
+    const question = interaction.options.getString('question');
+    const options = [];
+
+    // Extracting options from the interaction
+    for (let i = 0; i < letters.length; i++) {
+        const option = interaction.options.getString(`choice_${letters[i]}`);
+        if (option) {
+            options.push(option);
+        }
+    }
+
+    // If there are less than 2 options, create a simple poll with thumbs up and thumbs down reactions
+    if (options.length < 2) {
+        const pollMessage = await interaction.reply({
+            content: `📊 **${question}**`,
+            fetchReply: true
+        });
+        await pollMessage.react('👍');
+        await pollMessage.react('👎');
+        return;
+    }
+
+    // Creating the poll message with options represented by letter emojis
+    const optionsString = options.map((option, index) => `${emojis[index]} ${option}`).join('\n');
+
+    const fields = options.map((option, index) => ({ name: emojis[index], value: option }));
+
+    const embed = new EmbedBuilder()
+        .setColor(Colors.Blurple)
+        .setDescription('React with the corresponding letter to answer !')
+        .setColor('#0099ff')
+        .setFields(fields);
+
+
+    const pollMessage = await interaction.reply({
+        embeds: [embed],
+        fetchReply: true
+    });
+
+    // Reacting with letter emojis to the poll message
+    for (let i = 0; i < options.length; i++) {
+        await pollMessage.react(emojis[i]);
+    }
+}
